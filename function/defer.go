ee08028298f3584d8affef0e43c5b3085d9b753a
pojozhang <pojo.zhang@gmail.com>
2017-08-09 15:48:01 +0800

    update

--- function/defer.go
+++ function/defer.go
@@ -18,3 +18,22 @@ func trace() func() {
 		log.Println(time.Since(start))
 	}
 }
+
+func init() {
+	sum(1, 2, 3)
+}
+
+func sum(values ...int) (result int) {
+	defer log.Printf("result: %d\n", result)
+	for _, v := range values {
+		result += v
+	}
+	return
+}
+
+func init(){
+	defer log.Println("1")
+	defer log.Println("2")
+	defer log.Println("3")
+	panic("crash")
+}
