c9ebf85ca61d1988196ca9245a7973dfbd25be5d
pojozhang <pojo.zhang@gmail.com>
2017-08-10 13:58:21 +0800

    update

--- struct/struct.go
+++ struct/struct.go
@@ -12,15 +12,31 @@ type Employee struct {
 	Birth   time.Time
 }
 
+func (e *Employee) resetName() {
+	e.Name = "unknown"
+}
+
 type Color struct{ Red, Green, Blue int }
 
 type Point struct {
 	X, Y int
 }
 
+func (p *Point) reset() {
+	p.X = 0
+	p.Y = 0
+}
+
 type Circle struct {
 	Point
 	Radius int
+}
+
+type Rocket struct {
+}
+
+func (r *Rocket) Launch() {
+	fmt.Println("launch")
 }
 
 func main() {
@@ -31,6 +47,9 @@ func main() {
 	var address *string = &employee.Address
 	*address = "bbb"
 	fmt.Println(address)
+	fmt.Println(employee)
+	resetEmployeeName := employee.resetName
+	resetEmployeeName() //equals to employee.resetName()
 	fmt.Println(employee)
 
 	employeePtr := &employee
@@ -49,5 +68,15 @@ func main() {
 	circle := new(Circle)
 	circle.X = 3
 	circle.Y = 4
+	circle.reset() //继承Point的方法
+
 	fmt.Println(*circle)
+
+	(&Point{1, 2}).reset() //compile error if Point{1,2}.reset()
+
+	rocket := new(Rocket)
+	(*Rocket).Launch(rocket)
+	time.AfterFunc(1*time.Second, rocket.Launch)
+	//equals to below
+	time.AfterFunc(1*time.Second, func() { rocket.Launch() })
 }
