5ab7cbb991bbf24a9b2a96756ae3e0475496daec
pojozhang <pojo.zhang@gmail.com>
2017-08-16 17:18:55 +0800

    update

--- interface/interface.go
+++ interface/interface.go
@@ -18,10 +18,27 @@ func (w MailWriter) Write(text string) {
 	fmt.Println("write " + text)
 }
 
+type WriteFunc func(string)
+
+func (w WriteFunc) Write(text string) {
+	w(text)
+}
+
 func main() {
 	var writer Writer
 	writer = new(MailWriter)
 	fmt.Println(writer)
 	writer.Write("hello")
-	fmt.Printf("%T\n", writer)//打印接口值的动态类型
+	fmt.Printf("%T\n", writer) //打印接口值的动态类型
+
+	//让函数值实现接口->起到适配器作用
+	doWrite(WriteFunc(write), "func")
+}
+
+func doWrite(w Writer, s string) {
+	w.Write(s)
+}
+
+func write(s string) {
+	fmt.Println(s)
 }
