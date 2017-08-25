b87b0874f39793a93ba1163c078a59ae91469d2f
pojozhang <pojo.zhang@gmail.com>
2017-08-25 19:26:34 +0800

    update

--- interface/interface.go
+++ interface/interface.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 type Writer interface {
@@ -18,6 +21,13 @@ func (w MailWriter) Write(text string) {
 	fmt.Println("write " + text)
 }
 
+type PaperWriter struct {
+}
+
+func (w PaperWriter) Write(text string) {
+	fmt.Println("write " + text)
+}
+
 type WriteFunc func(string)
 
 func (w WriteFunc) Write(text string) {
@@ -25,14 +35,24 @@ func (w WriteFunc) Write(text string) {
 }
 
 func main() {
-	var writer Writer
-	writer = new(MailWriter)
+	var writer Writer = &MailWriter{}
 	fmt.Println(writer)
 	writer.Write("hello")
 	fmt.Printf("%T\n", writer) //打印接口值的动态类型
 
 	//让函数值实现接口->起到适配器作用
 	doWrite(WriteFunc(write), "func")
+
+	//类型断言
+	w2 := writer.(Writer)
+	w2.Write("w2")
+
+	//类型断言，注意，此处判断的是实现了某个接口的某个具体类型，I.(T) 类型T必须实现了接口I，否则编译报错
+	var w3 Writer = &MailWriter{}
+	if f, ok := w3.(*PaperWriter); !ok {//ok为false，因为w3是*MailWriter类型
+		fmt.Println(f)
+		fmt.Println("not match")
+	}
 }
 
 func doWrite(w Writer, s string) {
