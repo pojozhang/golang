ae5b03c607f5c25b3b15e1d60d6e62e1489d9b11
pojozhang <pojo.zhang@gmail.com>
2017-08-30 15:33:18 +0800

    update

---
+++ src/example/reflect/reflect.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println(reflect.TypeOf(3))
+	
+	type company struct{
+		name string
+		address string
+	}
+
+	c:=company{}
+	fmt.Println(reflect.ValueOf(c).Type().Field(0).Name)
+
+	n:=2
+	reflect.ValueOf(&n).Elem().SetInt(3)
+	fmt.Println(n)
+}
